fcbb98399ce504227e5a3b16e56c61426d0595b5
USHA NANDHINI <gushanandhini2004@gmail.com>
2024-06-03 11:54:41 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
     class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
 
     <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-        <a href="#">
+        <a href="https://ushaganeshdurai.github.io/SIMPLE-CALCULATOR/">
             <img src='https://cms.involve.me/blog/how-to-create-a-simple-price-calculator-and-capture-more-leads/calculator-L.png'
                     alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
@@ -23,7 +23,7 @@ const Projects = () => {
     </div>
 
     <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-        <a href="#">
+        <a href="https://reactbookstore-frontend.vercel.app/">
             <img src='https://media1.giphy.com/media/ZcdDG0yjF6XmoQ2xLD/giphy.gif?cid=6c09b952szi4ie05cx82twsxcqyjussp31o33lda8rei8kaw&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=s'
                     alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
@@ -37,7 +37,7 @@ const Projects = () => {
     </div>
 
     <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-        <a href="#">
+        <a href="https://www.figma.com/design/QJI4MmljHy722Uxvo47XnK/first-design?node-id=0-1">
             <img src="https://s3-alpha.figma.com/hub/file/1166690750/85f69649-5387-44c2-ba45-81ae13812e36-cover.png"
                     alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
@@ -51,7 +51,7 @@ const Projects = () => {
     </div>
   
     <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-        <a href="#">
+        <a href="https://ushaganeshdurai.github.io/dictionary-app/">
             <img src="https://cdn.hswstatic.com/gif/dictionary.jpg"
                     alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
@@ -63,7 +63,7 @@ const Projects = () => {
         </a>
     </div>
     <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-        <a href="#">
+        <a href="https://github.com/ushaganeshdurai/ecommerce">
             <img src="https://www.zilliondesigns.com/blog/wp-content/uploads/EcommerceLogos.jpg"
                     alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
@@ -75,7 +75,7 @@ const Projects = () => {
         </a>
     </div>
     <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-        <a href="#">
+        <a href="https://github.com/ushaganeshdurai/snapgram">
             <img src="https://www.dotcominfoway.com/wp-content/uploads/2020/01/social-media-app-cost.png"
                     alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">

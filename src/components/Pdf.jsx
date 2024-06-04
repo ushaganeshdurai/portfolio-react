ea2df13d82aeae3e30d4b565284e33cf0e50e150
USHA NANDHINI <gushanandhini2004@gmail.com>
2024-06-04 15:20:14 +0530

    Update Pdf.jsx

--- src/components/Pdf.jsx
+++ src/components/Pdf.jsx
@@ -4,7 +4,7 @@ const Pdf = () => {
   return (
     <>
     <div className='m-5 text-white h-full w-full font-roboto'>
-      <iframe src="https://ik.imagekit.io/tpkbnlhpp/USHA%20NANDHINI%20G_RESUME.pdf?updatedAt=1714709217970" width="100%" height="1000px" frameborder="0" allowfullscreen></iframe>
+      <iframe src="https://ik.imagekit.io/tpkbnlhpp/USHA%20NANDHINI%20G_RESUME.pdf?updatedAt=1717494568100" width="100%" height="1000px" frameborder="0" allowfullscreen></iframe>
     </div>
     </>
   );

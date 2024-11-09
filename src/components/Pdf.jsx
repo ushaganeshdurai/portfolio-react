695d9d638ab77b9c1cf0f5b6f0ccdafea1194d83
USHA NANDHINI <gushanandhini2004@gmail.com>
2024-11-09 15:36:37 +0000

    Updated projects and resume

--- src/components/Pdf.jsx
+++ src/components/Pdf.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import Navbar from './Navbar';
 
 
 const Pdf = () => {
   return (
     <>
-    <Navbar />
     <div className='m-5 text-white h-full w-full font-roboto'>
-      <iframe src="https://ik.imagekit.io/tpkbnlhpp/USHA%20NANDHINI%20G_RESUME.pdf?updatedAt=1717494568100" width="100%" height="1000px" frameborder="0" allowfullscreen></iframe>
+      <iframe src="https://res.cloudinary.com/dm3rs6xh4/image/upload/f_auto,q_auto/pbvrdecrx2amnmbnlsbl" width="100%" height="1000px" frameborder="0" allowfullscreen></iframe>
     </div>
     </>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -65,11 +65,23 @@ const Projects = () => {
         </a>
     </div>
     <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-        <a href="https://zenplas.vercel.app">
+        <a href="https://zenplas.in">
             <img src="https://ik.imagekit.io/tpkbnlhpp/logo-removebg-preview.png?updatedAt=1720878607334?tr=f-webp"
                     alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
                 <p class="text-lg font-bold text-black truncate block capitalize">ZenPlas</p>
+                <div class="flex items-center">
+                    <p class="text-lg font-semibold text-gray-600 cursor-auto my-3">Freelance Project</p>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+        <a href="https://cake-wheel.vercel.app">
+            <img src="https://ik.imagekit.io/tpkbnlhpp/logo-removebg-preview.png?updatedAt=1720878607334?tr=f-webp"
+                    alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+            <div class="px-4 py-3 w-72">
+                <p class="text-lg font-bold text-black truncate block capitalize">Cakewheel</p>
                 <div class="flex items-center">
                     <p class="text-lg font-semibold text-gray-600 cursor-auto my-3">Freelance Project</p>
                 </div>
@@ -89,13 +101,25 @@ const Projects = () => {
         </a>
     </div>
     <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-        <a href="https://github.com/ushaganeshdurai/appointmentbookingsystem">
+        <a href="https://github.com/ushaganeshdurai/appointmentbookingsystem-frontend.vercel.app">
             <img src="https://camo.githubusercontent.com/aa823fe2118937109c8f98d3d9214cf682a5a7a066266107a9fdfb360edb7f28/68747470733a2f2f696b2e696d6167656b69742e696f2f74706b626e6c6870702f53637265656e73686f74253230323032342d30362d31332532303134323430362e706e673f7570646174656441743d31373138323638383830333337"
                     alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
                 <p class="text-lg font-bold text-black truncate block capitalize">Appointment Booking <br /> System</p>
                 <div class="flex items-center">
                     <p class="text-lg font-semibold text-gray-600 cursor-auto my-3">React js</p>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+        <a href="https://github.com/ushaganeshdurai/appointmentbookingsystem-frontend.vercel.app">
+            <img src="https://camo.githubusercontent.com/aa823fe2118937109c8f98d3d9214cf682a5a7a066266107a9fdfb360edb7f28/68747470733a2f2f696b2e696d6167656b69742e696f2f74706b626e6c6870702f53637265656e73686f74253230323032342d30362d31332532303134323430362e706e673f7570646174656441743d31373138323638383830333337"
+                    alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+            <div class="px-4 py-3 w-72">
+                <p class="text-lg font-bold text-black truncate block capitalize">Personal Expense <br /> Tracker</p>
+                <div class="flex items-center">
+                    <p class="text-lg font-semibold text-gray-600 cursor-auto my-3">Sveltekit</p>
                 </div>
             </div>
         </a>
